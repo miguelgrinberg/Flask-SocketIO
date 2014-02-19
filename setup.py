22d21efa52a90a0715b4dd5ed59323f2c92ec033
Miguel Grinberg <miguelgrinberg50@gmail.com>
2014-02-19 07:50:57 -0800

    version 0.2.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(
     name='Flask-SocketIO',
-    version='0.2.1',
+    version='0.2.2',
     url='http://github.com/miguelgrinberg/Flask-SocketIO/',
     license='MIT',
     author='Miguel Grinberg',
