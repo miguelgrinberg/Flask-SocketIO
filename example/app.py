2c74544fe16efadfa7691abc1d36b1e718efc34a
Miguel Grinberg <miguelgrinberg50@gmail.com>
2015-08-20 00:33:02 -0700

    Minor updates to example application

--- example/app.py
+++ example/app.py
@@ -8,7 +8,6 @@ from flask_socketio import SocketIO, emit, join_room, leave_room, \
     close_room, rooms, disconnect
 
 app = Flask(__name__)
-app.debug = True
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app)
 thread = None
