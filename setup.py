965b30a6dc83b570c05925ef9acee68ae57aadff
Miguel Grinberg <miguelgrinberg50@gmail.com>
2016-02-21 10:11:52 -0800

    Added missing Python version classifiers to package

--- setup.py
+++ setup.py
@@ -35,6 +35,8 @@ setup(
         'License :: OSI Approved :: MIT License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3',
         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
         'Topic :: Software Development :: Libraries :: Python Modules'
     ]
