9086588452078024d0ed2f9532de63bf16f5194f
Miguel Grinberg <miguelgrinberg50@gmail.com>
2016-06-28 10:27:24 -0700

    Improvements to example application
    - greatly simplified handling of async modes
    - removed monkey patching and started using generic thread and sleep
      functions
    - added latency measurements with ping/pong messages

--- example/app.py
+++ example/app.py
@@ -1,43 +1,12 @@
 #!/usr/bin/env python
-
-# Set this variable to "threading", "eventlet" or "gevent" to test the
-# different async modes, or leave it set to None for the application to choose
-# the best option based on available packages.
-async_mode = None
-
-if async_mode is None:
-    try:
-        import eventlet
-        async_mode = 'eventlet'
-    except ImportError:
-        pass
-
-    if async_mode is None:
-        try:
-            from gevent import monkey
-            async_mode = 'gevent'
-        except ImportError:
-            pass
-
-    if async_mode is None:
-        async_mode = 'threading'
-
-    print('async_mode is ' + async_mode)
-
-# monkey patching is necessary because this application uses a background
-# thread
-if async_mode == 'eventlet':
-    import eventlet
-    eventlet.monkey_patch()
-elif async_mode == 'gevent':
-    from gevent import monkey
-    monkey.patch_all()
-
-import time
-from threading import Thread
 from flask import Flask, render_template, session, request
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
     close_room, rooms, disconnect
+
+# Set this variable to "threading", "eventlet" or "gevent" to test the
+# different async modes, or leave it set to None for the application to choose
+# the best option based on installed packages.
+async_mode = None
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
@@ -49,7 +18,7 @@ def background_thread():
     """Example of how to send server generated events to clients."""
     count = 0
     while True:
-        time.sleep(10)
+        socketio.sleep(10)
         count += 1
         socketio.emit('my response',
                       {'data': 'Server generated event', 'count': count},
@@ -60,10 +29,8 @@ def background_thread():
 def index():
     global thread
     if thread is None:
-        thread = Thread(target=background_thread)
-        thread.daemon = True
-        thread.start()
-    return render_template('index.html')
+        thread = socketio.start_background_task(target=background_thread)
+    return render_template('index.html', async_mode=socketio.async_mode)
 
 
 @socketio.on('my event', namespace='/test')
@@ -122,6 +89,11 @@ def disconnect_request():
     emit('my response',
          {'data': 'Disconnected!', 'count': session['receive_count']})
     disconnect()
+
+
+@socketio.on('my ping', namespace='/test')
+def ping_pong():
+    emit('my pong')
 
 
 @socketio.on('connect', namespace='/test')
