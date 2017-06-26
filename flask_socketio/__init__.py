a05acd2b491a79ae7a550f6bdff89030ccab2012
Miguel Grinberg <miguel.grinberg@gmail.com>
2017-06-25 18:55:07 -0700

    Release 2.9.0

--- flask_socketio/__init__.py
+++ flask_socketio/__init__.py
@@ -22,7 +22,7 @@ from werkzeug.serving import run_with_reloader
 from .namespace import Namespace
 from .test_client import SocketIOTestClient
 
-__version__ = '2.8.6'
+__version__ = '2.9.0'
 
 
 class _SocketIOMiddleware(socketio.Middleware):
