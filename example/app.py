8fae433e1d0da730f39f5f7c1fd941664f7e6f4f
Muhammad Hamza <muhammadhs@pm.me>
2019-01-07 21:03:00 +0500

    spelling corrected

--- example/app.py
+++ example/app.py
@@ -6,7 +6,7 @@ from flask_socketio import SocketIO, emit, join_room, leave_room, \
 
 # Set this variable to "threading", "eventlet" or "gevent" to test the
 # different async modes, or leave it set to None for the application to choose
-# the best option gased on installed packages.
+# the best option based on installed packages.
 async_mode = None
 
 app = Flask(__name__)
