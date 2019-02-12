5585f9886bf769d7d54fd67e44fe6e87e8fb2501
Miguel Grinberg <miguel.grinberg@gmail.com>
2019-02-12 17:28:16 +0000

    Release 3.2.2

--- flask_socketio/__init__.py
+++ flask_socketio/__init__.py
@@ -24,7 +24,7 @@ from werkzeug.serving import run_with_reloader
 from .namespace import Namespace
 from .test_client import SocketIOTestClient
 
-__version__ = '3.2.1'
+__version__ = '3.2.2'
 
 
 class _SocketIOMiddleware(socketio.WSGIApp):
