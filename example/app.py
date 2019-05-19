f8ee01f938afcd13d4cae282976946660db8e982
Miguel Grinberg <miguel.grinberg@gmail.com>
2019-05-19 15:29:31 +0100

    example app: disconnect in callback function (fixes #453)

--- example/app.py
+++ example/app.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 from threading import Lock
-from flask import Flask, render_template, session, request
+from flask import Flask, render_template, session, request, \
+    copy_current_request_context
 from flask_socketio import SocketIO, emit, join_room, leave_room, \
     close_room, rooms, disconnect
 
@@ -84,10 +85,17 @@ def send_room_message(message):
 
 @socketio.on('disconnect_request', namespace='/test')
 def disconnect_request():
+    @copy_current_request_context
+    def can_disconnect():
+        disconnect()
+
     session['receive_count'] = session.get('receive_count', 0) + 1
+    # for this emit we use a callback function
+    # when the callback function is invoked we know that the message has been
+    # received and it is safe to disconnect
     emit('my_response',
-         {'data': 'Disconnected!', 'count': session['receive_count']})
-    disconnect()
+         {'data': 'Disconnected!', 'count': session['receive_count']},
+         callback=can_disconnect)
 
 
 @socketio.on('my_ping', namespace='/test')
