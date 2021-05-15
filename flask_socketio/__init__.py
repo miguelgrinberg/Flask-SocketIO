8146095abdf0a7ec8a979e8504193a25692d0a3d
Miguel Grinberg <miguel.grinberg@gmail.com>
2021-05-15 15:14:23 +0100

    Version 5.0.4dev

--- flask_socketio/__init__.py
+++ flask_socketio/__init__.py
@@ -26,7 +26,7 @@ from werkzeug._reloader import run_with_reloader
 from .namespace import Namespace
 from .test_client import SocketIOTestClient
 
-__version__ = '5.0.3'
+__version__ = '5.0.4dev'
 
 
 class _SocketIOMiddleware(socketio.WSGIApp):
